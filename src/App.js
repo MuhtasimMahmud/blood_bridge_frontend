de7bfb1e5eb26374e1db0e2acc0707b4b98232a0
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2024-08-31 04:04:55 +0600

    uploaded

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/> 
+      <h1> Hello world! Hi</h1>
+      <p> Hello </p>
+    </div>
+  );
+}
+
+export default App;
